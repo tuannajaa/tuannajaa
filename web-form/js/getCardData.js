b9541471ff3ad8fe9ec28c60869ccb69c16d1f49
tuannajaa <88039184+tuannajaa@users.noreply.github.com>
2021-08-06 22:22:39 +0700

    ดึงข้อมูลบัตร ผู้ร้อง พยาน1 พยาน2

--- web-form/js/getCardData.js
+++ web-form/js/getCardData.js
@@ -28,43 +28,6 @@ function getCardData() {
 
         });
 }
-
-function getFormatedDateOfBirth(dateOfBirth){
-    var year = dateOfBirth.slice(0,4)
-    var month = dateOfBirth.slice(4,6)
-    var date = dateOfBirth.slice(6,8)
-    
-    return date+"/"+month+"/"+year
-}
-function getFormatedDateOfBirth(issueDate){
-    var yearIssue = issueDate.slice(0,4)
-    var monthIssue = issueDate.slice(4,6)
-    var dateIssue = issueDate.slice(6,8)
-    
-    return dateIssue+"/"+monthIssue+"/"+yearIssue
-}
-function getFormatedDateOfBirth(expDate){
-    var yearExpire = expDate.slice(0,4)
-    var monthExpire = expDate.slice(4,6)
-    var dateExpire = expDate.slice(6,8)
-    
-    return dateExpire+"/"+monthExpire+"/"+yearExpire
-}
-
-function getAddress(address){
-    addressArray = address.split("#")
-    var data ={
-        addressNo : addressArray[0],
-        moo : addressArray[1],
-        soi : addressArray[2],
-        road : addressArray[3],
-        tambol : addressArray[5],
-        district : addressArray[6],
-        provice : addressArray[7],
-    }
-
-    return data
-}
 //////////////////// GetCardData For Mother /////////////////
 function getCardDataMom() {
     console.log("function is working")
@@ -92,36 +55,97 @@ function getCardDataMom() {
             let cutDistrict = district.replace(/อำเภอ|เขต/g,"")                          
             document.getElementById("district_mom").value = cutDistrict
             document.getElementById("provice_mom").value = addressData.provice.substring(7) 
-            console.log(addressData.moo.substring(7))
+            
 
         });
 }
+//////////////////// GetCardData For Wit1 /////////////////
+function getCardDataWit1() {
+    console.log("function is working")
+    fetch('https://localhost:8443/smartcard/data/')
+        .then(response => response.json())
+        .then(data => {
 
+            let dateOfBirth = getFormatedDateOfBirth(data.dob)
+            let issueDate = getFormatedDateOfBirth(data.issue_date)
+            let expDate = getFormatedDateOfBirth(data.expire_date)
+            let preN = data.prename
+            let repreN = preN.replace(/น.ส./g,"นางสาว")
+            document.getElementById("full_name_wit1").value = repreN+data.fname+" "+data.lname
+            document.getElementById("birthday_wit1").value = dateOfBirth
+            document.getElementById("card_wit1").value = data.cid
+            document.getElementById("init-card_wit1").value = issueDate
+            document.getElementById("exp-card_wit1").value = expDate
+
+            let addressData = getAddress(data.address)
+            document.getElementById("addresss_wit1").value = addressData.addressNo
+            document.getElementById("moo_wit1").value = addressData.moo.substring(7)         //ตัดคำว่า หมู่ที่ ออก
+            document.getElementById("road_wit1").value = addressData.road
+            document.getElementById("tambol_wit1").value = addressData.tambol.substring(4)   //ตัด ตำบล แขวง
+            let district = addressData.district                                         //ตัดคำ อำเภอ และ เขต
+            let cutDistrict = district.replace(/อำเภอ|เขต/g,"")                          
+            document.getElementById("district_wit1").value = cutDistrict
+            document.getElementById("provice_wit1").value = addressData.provice.substring(7) 
+            
+
+        });
+}
+//////////////////// GetCardData For Wit2 /////////////////
+function getCardDataWit2() {
+    console.log("function is working")
+    fetch('https://localhost:8443/smartcard/data/')
+        .then(response => response.json())
+        .then(data => {
+
+            let dateOfBirth = getFormatedDateOfBirth(data.dob)
+            let issueDate = getFormatedDateOfBirth(data.issue_date)
+            let expDate = getFormatedDateOfBirth(data.expire_date)
+            let preN = data.prename
+            let repreN = preN.replace(/น.ส./g,"นางสาว")
+            document.getElementById("full_name_wit2").value = repreN+data.fname+" "+data.lname
+            document.getElementById("birthday_wit2").value = dateOfBirth
+            document.getElementById("card_wit2").value = data.cid
+            document.getElementById("init-card_wit2").value = issueDate
+            document.getElementById("exp-card_wit2").value = expDate
+
+            let addressData = getAddress(data.address)
+            document.getElementById("addresss_wit2").value = addressData.addressNo
+            document.getElementById("moo_wit2").value = addressData.moo.substring(7)         //ตัดคำว่า หมู่ที่ ออก
+            document.getElementById("road_wit2").value = addressData.road
+            document.getElementById("tambol_wit2").value = addressData.tambol.substring(4)   //ตัด ตำบล แขวง
+            let district = addressData.district                                         //ตัดคำ อำเภอ และ เขต
+            let cutDistrict = district.replace(/อำเภอ|เขต/g,"")                          
+            document.getElementById("district_wit2").value = cutDistrict
+            document.getElementById("provice_wit2").value = addressData.provice.substring(7) 
+            
+
+        });
+}
 function getFormatedDateOfBirth(dateOfBirth){
-    let year = dateOfBirth.slice(0,4)
-    let month = dateOfBirth.slice(4,6)
-    let date = dateOfBirth.slice(6,8)
+    var year = dateOfBirth.slice(0,4)
+    var month = dateOfBirth.slice(4,6)
+    var date = dateOfBirth.slice(6,8)
     
     return date+"/"+month+"/"+year
 }
 function getFormatedDateOfBirth(issueDate){
-    let yearIssue = issueDate.slice(0,4)
-    let monthIssue = issueDate.slice(4,6)
-    let dateIssue = issueDate.slice(6,8)
+    var yearIssue = issueDate.slice(0,4)
+    var monthIssue = issueDate.slice(4,6)
+    var dateIssue = issueDate.slice(6,8)
     
     return dateIssue+"/"+monthIssue+"/"+yearIssue
 }
 function getFormatedDateOfBirth(expDate){
-    let yearExpire = expDate.slice(0,4)
-    let monthExpire = expDate.slice(4,6)
-    let dateExpire = expDate.slice(6,8)
+    var yearExpire = expDate.slice(0,4)
+    var monthExpire = expDate.slice(4,6)
+    var dateExpire = expDate.slice(6,8)
     
     return dateExpire+"/"+monthExpire+"/"+yearExpire
 }
 
 function getAddress(address){
     addressArray = address.split("#")
-    let data ={
+    var data ={
         addressNo : addressArray[0],
         moo : addressArray[1],
         soi : addressArray[2],
