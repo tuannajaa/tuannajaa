32ed9b07acd45d94431cac9d763861cbe0b7bd23
tuannajaa <88039184+tuannajaa@users.noreply.github.com>
2022-02-10 11:14:08 +0700

    test validated input text

---
+++ web-form/js/mycheck.js
@@ -0,0 +1,12 @@
+function mycheck() {
+    var text0 = document.getElementById('officer');
+    if (text0.value !=''){
+        document.getElementById('officer').style.backgroundColor = "#e4ffdf"
+    }
+    
+    var text1 = document.getElementById('docNo');
+    if (text1.value !=''){
+        document.getElementById('docNo').style.backgroundColor = "#e4ffdf"
+    }
+    
+}
